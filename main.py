6198661f3abbe67a4aa14dee6c3d7ac704d8261d
JaimeMatias <matias.raul.jaime@gmail.com>
2018-06-03 14:43:41 -0300

    Segundo commit

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-  return 'Hey its Python Flask application!'
+  return 'Hola es una aplicación de Python!'
 
 if __name__ == '__main__':
   app.run()
