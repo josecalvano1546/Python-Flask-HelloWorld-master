67324a619f7cdec1d6df47a7d3039e07ad68caad
JaimeMatias <matias.raul.jaime@gmail.com>
2018-06-05 23:16:19 -0300

    prueba 1

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-  return 'Hola es la aplicación de Python!'
+  return 'Hola es una aplicación de Python!'
 
 if __name__ == '__main__':
   app.run()
