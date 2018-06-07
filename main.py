9f0a057b10356df9506223e853ddf3a6896991c7
JaimeMatias <matias.raul.jaime@gmail.com>
2018-06-07 13:21:23 -0300

    prueba codeship 8

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import clasehola
 
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-      return 'Hola es una aplicación de Pythones!'
-      
+def hello():
+      return clasehola.hello_world()
+
 if __name__ == '__main__':
   app.run()
