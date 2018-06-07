3019f902dc9745031d1e94d4baac884431521760
JaimeMatias <matias.raul.jaime@gmail.com>
2018-06-07 13:20:19 -0300

    prueba codeship 7

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-    def hello_world():
+def hello_world():
       return 'Hola es una aplicación de Pythones!'
+      
 if __name__ == '__main__':
   app.run()
