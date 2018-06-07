aabc70b986b2df41cf3259a29b15a27e99277239
JaimeMatias <matias.raul.jaime@gmail.com>
2018-06-07 13:12:26 -0300

    prueba codeship 3

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import clasehola
+
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
+    import clasehola
     clasehola.hello_world()
 if __name__ == '__main__':
   app.run()
