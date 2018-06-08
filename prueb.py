430d9abbb1ffd00f22f74a30b65b14c939c34002
JaimeMatias <matias.raul.jaime@gmail.com>
2018-06-08 13:59:18 -0300

    prueba codeship 10

--- prueb.py
+++ prueb.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import unittest
-import clasehola
-
+import interfaz
 class TestMyModule(unittest.TestCase):
 
     def test_interfaz(self):
-        self.assertEqual(clasehola.hello_world(), 'Hola es una aplicación de Python!')
+        self.assertEqual(interfaz.hello_world(), 'Hola es una aplicación de Python!')
 if __name__ == "__main__":
     unittest.main()
--- test_mymodule.py
+++ test_mymodule.py
@@ -5,6 +5,6 @@ import mymodule
 class TestMyModule(unittest.TestCase):
 
     def test_sum(self):
-        self.assertEqual(mymodule.sum(5, 7), 12)
+        self.assertEqual(mymodule.sum(5, 6), 12)
 if __name__ == "__main__":
     unittest.main()
